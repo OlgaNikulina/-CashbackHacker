eabc903cf9b1fae821eb12ca53b03428b3876c44
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-20 18:50:03 +0500

    method remain and tests added in junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,49 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+
 
     @Test
-    void shouldCalculateRemain() {
+    public void shouldCalculate100() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculate0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
         assertEquals(expected, actual);
     }
 }
