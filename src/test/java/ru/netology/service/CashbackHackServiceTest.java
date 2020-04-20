9cd1400512ddeae69b00157331f40f9ba848376f
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-20 18:52:51 +0500

    modidifcations

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,12 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateRemain() {
+    void shouldCalculate100() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculate500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculate1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculate0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
         assertEquals(expected, actual);
     }
 }
