659c0e889b7574abf0ffc65902c4e669b3d81535
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-20 17:43:53 +0500

    service added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldCalculateRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+}
